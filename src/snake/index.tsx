b6230e7c57585452af6bcf196263512719f6d74c
Tronikel <56039679+Trunkelis@users.noreply.github.com>
2021-08-10 13:51:38 +0300

    movement is broken, set previous is janky idk

--- src/snake/index.tsx
+++ src/snake/index.tsx
@@ -1,10 +1,17 @@
 import { FC, useState, useEffect } from "react";
 
 import { usePreviousNumber } from "react-hooks-use-previous";
+import useEventListener from "@use-it/event-listener";
 
 import Board from "./board";
 
+// movement types
+type Movement = "forwards" | "backwards" | "left" | "right";
+
 const Snake: FC = () => {
+
+    // current movement state
+    const [moving, setMoving] = useState<Movement>("right");
 
     // snake pos state
     const [posX, setPosX] = useState(1);
@@ -17,12 +24,66 @@ const Snake: FC = () => {
     // every 0.5 second move the snake 
     useEffect(() => {
         const interval = setInterval(() => {
-            setPosX(prev => prev + 1);
-        }, 1000);
+
+           
+            // handle which way are we moving
+            switch (moving) {
+                case "forwards":
+                    setPosY(prev => prev - 1);
+                    break;
+                
+                case "backwards":
+                    setPosY(prev => prev + 1);
+                    break;
+                
+                case "right":
+                    setPosX(prev => prev + 1);
+                    break;
+                
+                case "left":
+                    setPosX(prev => prev - 1);
+            };
+
+            
+        }, 500);
 
         // no memory leak
         return () => clearInterval(interval);
-    }, []);
+    }, [moving]);
+
+    console.log({
+        cur: {
+            x: posX,
+            y: posY,
+        },
+        prev: {
+            x: prevX,
+            y: posY,
+        },
+    });
+
+    // handle movement
+    useEventListener("keydown", (event: KeyboardEvent) => {
+
+        // w = 87
+        // a = 65
+        // s = 83
+        // d = 68
+        switch (event.keyCode) {
+            case 87:
+                setMoving("forwards");
+                break;
+            case 65:
+                setMoving("left");
+                break;
+            case 83:
+                setMoving("backwards");
+                break;
+            case 68:
+                setMoving("right");
+                break;
+        };
+    });
 
     return (
         <Board
